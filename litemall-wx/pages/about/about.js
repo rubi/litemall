c72cb8c921db07ee92f5632f6080f4eda15e326e
seven <juanleyiji@gmail.com>
2019-11-22 14:05:17 +0800

    fix location issue

--- litemall-wx/pages/about/about.js
+++ litemall-wx/pages/about/about.js
@@ -6,44 +6,45 @@ var util = require("../../utils/util.js");
 var api = require("../../config/api.js");
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    load_statue: true,
-    shopInfo: {
-      name: 'yltmall',
-      address: 'https://github.com/linlinjava/litemall',
-      latitude: 31.201900,
-      longitude: 121.587839,
-      linkPhone: '021-xxxx-xxxx',
-      qqNumber: '738696120'
-    },
-  },
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		load_statue: true,
+		shopInfo: {
+			name: '襄阳永力通机械有限公司',
+			address: '襄阳市高新区邓城大道邓城工业园',
+			latitude: 32.099734,
+			longitude: 112.106235,
+			linkPhone: '0710-3725679',
+			qqNumber: '1242607804',
+			version: '1.0.7'
+		},
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
 
-  showLocation: function (e) {
-    var that = this
-    wx.openLocation({
-      latitude: that.data.shopInfo.latitude,
-      longitude: that.data.shopInfo.longitude,
-      name: that.data.shopInfo.name,
-      address: that.data.shopInfo.address,
-    })
-  },
-  callPhone: function (e) {
-    var that = this
-    wx.makePhoneCall({
-      phoneNumber: that.data.shopInfo.linkPhone,
-    })
-  },
-  reLoad: function (e) {
-    this.loadShopInfo();
-  }
+	},
+
+	showLocation: function (e) {
+		var that = this
+		wx.openLocation({
+			latitude: that.data.shopInfo.latitude,
+			longitude: that.data.shopInfo.longitude,
+			name: that.data.shopInfo.name,
+			address: that.data.shopInfo.address,
+		})
+	},
+	callPhone: function (e) {
+		var that = this
+		wx.makePhoneCall({
+			phoneNumber: that.data.shopInfo.linkPhone,
+		})
+	},
+	reLoad: function (e) {
+		this.loadShopInfo();
+	}
 })
